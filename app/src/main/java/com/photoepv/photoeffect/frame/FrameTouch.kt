111a269609a912613ecb2ccc204bc9d59c1ce793
asadali357201 <asadali357201@gmail.com>
2021-05-29 13:28:50 +0500

    first commit

---
+++ app/src/main/java/com/photoepv/photoeffect/frame/FrameTouch.kt
@@ -0,0 +1,6 @@
+package com.photoepv.photoeffect.frame
+
+abstract class FrameTouch : OnFrameTouchListener {
+    var isImageFrameMoving = false
+
+}
---
+++ app/src/main/java/com/photoepv/photoeffect/model/EffectData.kt
@@ -0,0 +1,17 @@
+package com.photoepv.photoeffect.model
+
+import com.google.gson.annotations.SerializedName
+
+class EffectData {
+    @SerializedName("name")
+    var name: String
+
+    @SerializedName("icon")
+    var icon: Int
+
+    constructor(name: String, icon: Int) {
+        this.name = name
+        this.icon = icon
+    }
+
+}
